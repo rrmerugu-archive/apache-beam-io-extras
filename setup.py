a7aa92d61e86e78b34752dff9c128cb1d3d7397c
Ravi RT Merugu <rrmerugu@gmail.com>
2018-04-06 03:33:45 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='apache-beam-io-extras',
+    version='0.0.1',
+    packages=['beam_io_extras'],
+    url='https://github.com/invanatech/apache-beam-python-ptransforms',
+    license='MIT License',
+    author='rrmerugu',
+    author_email='rrmerugu@gmail.com',
+    description='The missing I/O Transforms in python which already exist in Java SDK based on https://beam.apache.org/documentation/io/built-in/'
+)
